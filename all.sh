1b60b9049a80b74f4509f9531483d9c14b73e826
Andrew Meyer <andrewm.bpi@gmail.com>
2016-02-16 14:42:17 -0600

    Add noproxy alias

--- all.sh
+++ all.sh
@@ -1,6 +1,7 @@
 _DIR_=${BASH_SOURCE%/*}
 
 source $_DIR_/be.sh
+source $_DIR_/noproxy.sh
 source $_DIR_/prompt.sh
 source $_DIR_/up.sh
 
---
+++ noproxy.sh
@@ -0,0 +1,1 @@
+alias noproxy="env --unset=http_proxy --unset=https_proxy --unset=HTTP_PROXY --unset=HTTPS_PROXY"
