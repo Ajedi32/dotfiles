a601e4125af2d6c3c1d1b8bfb132d93ea1a0db54
Andrew Meyer <andrewm.bpi@gmail.com>
2021-08-04 14:44:47 -0500

    PowerShell: Use Complete instead of MenuComplete
    MenuComplete is pretty but in practice I think I still like the more bash-like Complete
    functionality better.

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -1,5 +1,5 @@
 # Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 # Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
