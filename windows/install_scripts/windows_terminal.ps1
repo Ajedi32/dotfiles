d4d25b4a825c590b6f08dc169fd173144d0aa89f
Andrew Meyer <andrewm.bpi@gmail.com>
2021-12-14 13:08:20 -0600

    Add install scripts for wsl and windows terminal

---
+++ windows/install_scripts/windows_terminal.ps1
@@ -0,0 +1,5 @@
+# Requires winget
+
+# Note: As of 2021-12-14 this is currently broken, as Windows won't let you
+# install stuff via the CLI without an account
+winget install --accept-package-agreements --accept-source-agreements --id 9n0dx20hk701 # Windows Terminal
