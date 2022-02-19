f60ddc92bf6beb3a287dc90921a6810b1f987f91
Andrew Meyer <andrewm.bpi@gmail.com>
2022-02-19 15:15:59 -0600

    Windows: Script for enabling ssh-agent

---
+++ windows/install_scripts/openssh_agent.ps1
@@ -0,0 +1,9 @@
+# By default the ssh-agent service is disabled. Allow it to be manually started for the next step to work.
+# Make sure you're running as an Administrator.
+Get-Service ssh-agent | Set-Service -StartupType Manual
+
+# Start the service
+Start-Service ssh-agent
+
+# This should return a status of Running
+Get-Service ssh-agent
